990e5d5030ee7e8db6be31c0e4ad337a79b44f52
CHEW CHEN SENG <isitcoder97@gmail.com>
2023-03-18 02:14:32 +0800

    Update to game

--- app/src/main/java/com/huawei/touchgame/MainActivity.java
+++ app/src/main/java/com/huawei/touchgame/MainActivity.java
@@ -8,10 +8,12 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.TextView;
-
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,30 +21,76 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Initialize the GridLayout
-        GridLayout gridLayout = findViewById(R.id.grid_layout);
-        int level =1;
 
-        for (int i = 0; i < 4; i++) {
+
+        int numViews=16;
+        createViews(numViews);
+        highlightNewView(numViews);
+        TextView scoreTextView = findViewById(R.id.score_text_view);
+        scoreTextView.setText("Score: " + score);
+
+
+    }
+
+
+    ArrayList<View> falseViews = new ArrayList<View>();
+
+
+    private void createViews(int numberView){
+
+        for (int i = 0; i < numberView; i++) {
             // Create a new View
             View view = new View(this);
-            // Set the background color to gray
-
             Drawable drawable = getDrawable(R.drawable.border);
             view.setBackground(drawable);
-            // Set the layout parameters for the View
+
             GridLayout.LayoutParams params = new GridLayout.LayoutParams();
             params.width = 0;
             params.height = 0;
-            params.columnSpec = GridLayout.spec(i/2,1f);
-            params.rowSpec = GridLayout.spec(i%2,1f);
+            params.columnSpec = GridLayout.spec(i / 2, 1f);
+            params.rowSpec = GridLayout.spec(i % 2, 1f);
+            view.setLayoutParams(params);
+            falseViews.add(view);
             // Add the View to the GridLayout
-            gridLayout.addView(view, params);
+            view.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    // Check if the touched View is the highlighted View
+                    if (v == highlightedView) {
+                        // Increment the score
+                        v.setTag(true);
+                        v.setBackgroundColor(Color.BLUE);
+                        falseViews.remove(v);
+
+
+
+                        score++;
+                        TextView scoreTextView = findViewById(R.id.score_text_view);
+                        scoreTextView.setText("Score: " + score);
+                        // Highlight a new View
+                        highlightNewView(numberView);
+                    }
+                    return true;
+                }
+            });
+
+            GridLayout gridLayout = findViewById(R.id.grid_layout);
+            gridLayout.addView(view);
         }
-
-        View highlightedView;
-        highlightedView = gridLayout.getChildAt(new Random().nextInt(4));
-        highlightedView.setBackgroundColor(Color.YELLOW);
-
     }
+
+
+
+
+    View highlightedView;
+    private void highlightNewView(int numViews) {
+
+        highlightedView = falseViews.get(new Random().nextInt(falseViews.size()));
+        highlightedView.setBackgroundColor(Color.YELLOW);
+    }
+
+    int count=0;
+
+
 
 }
